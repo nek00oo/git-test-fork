74933ac4f7edd02d27581b3ff8a5286689339d26
nek00oo <yakovenkovalera2017@yandex.ru>
2025-07-28 16:39:43 +0300

    fix

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def get_user_input():
     name = input("Enter your USERNAME PLEASE: ")
-    print("Commit#1")
+    print("Commit#3")
     length = float(input("Enter rectangle Length: "))
     width = float(input("Enter rectangle Width : "))
     return name, length, width
 
 if __name__ == "__main__":
     user_name, user_length, user_width = get_user_input()
-    print("I_MAster")
+    print("I_master")
     print("user_name, user_length, user_width")
     print("Area of rectangle:", calculate_area(6, 3))
