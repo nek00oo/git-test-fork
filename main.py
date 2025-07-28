965d16b3ec6db0380bcbab45b3b69883b05a2487
nek00oo <yakovenkovalera2017@yandex.ru>
2025-07-28 16:04:50 +0300

    FIX

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def get_user_input():
-    name = input("Enter your username: ")
-    length = float(input("Enter rectangle length: "))
-    width = float(input("Enter rectangle width : "))
+    name = input("Enter your USERNAME PLEASE: ")
+    length = float(input("Enter rectangle Length: "))
+    width = float(input("Enter rectangle Width : "))
     return name, length, width
 
 if __name__ == "__main__":
     user_name, user_length, user_width = get_user_input()
     print("user_name, user_length, user_width")
-    print("Area of rectangle:", calculate_area(5, 3))
+    print("Area of rectangle:", calculate_area(6, 3))
