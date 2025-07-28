50f3d38c05e9889b28b23b9e937ac9f7eb72df27
nek00oo <yakovenkovalera2017@yandex.ru>
2025-07-28 16:36:36 +0300

    I_master

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ def get_user_input():
 
 if __name__ == "__main__":
     user_name, user_length, user_width = get_user_input()
+    print("I_MAster")
     print("user_name, user_length, user_width")
     print("Area of rectangle:", calculate_area(6, 3))
